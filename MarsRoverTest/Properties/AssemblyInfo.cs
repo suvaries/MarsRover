93e4b8fdafaa17b3a818e71c9debd96bed77a94d
Serdar Suvarierol <serdar.suvarierol@gmail.com>
2020-02-13 21:16:03 +0000

    test project added

---
+++ MarsRoverTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MarsRoverTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SaBIS")]
+[assembly: AssemblyProduct("MarsRoverTest")]
+[assembly: AssemblyCopyright("Copyright © SaBIS 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f276d6a-54e2-4fce-8448-a01604f33d7e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
